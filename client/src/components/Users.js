b7e9c22cb9df4e637af03de383dfdb5f8bcb23d6
Archit Ghevariya <ghevariyaarchit@gmail.com>
2023-07-19 21:18:11 +0530

    creating userlist menu

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,31 +1,38 @@
 import React from "react";
-import { TextField, InputAdornment,Container } from "@mui/material";
+import { TextField, InputAdornment, Container } from "@mui/material";
 import { Search } from "lucide-react";
 import "../style/style.css";
 
 export default function Users() {
   return (
     <>
-    <Container className="UserList">
-      <p className="Heading">Messages</p>
-      <TextField
-        placeholder="Search..."
-        className="SearchBox"
-        size="small"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Search size="16" />
-            </InputAdornment>
-          ),
-        }}
-      />
-          <Container>
-        {/* <img /> */}
-        <p>Archit Ghevariya</p>
-    </Container>
-    </Container>
+      <Container className="UserList">
+        <p className="Heading">Messages</p>
+        <TextField
+          placeholder="Search..."
+          className="SearchBox"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search size="16" />
+              </InputAdornment>
+            ),
+          }}
+        />
 
+        <div className="UserDetails">
+          <img
+            className="profile-image"
+            src={require("../assets/imgs/profile.jpg")}
+            alt=""
+          />
+          <div className="user-info">
+            <p className="username">Archit Ghevariya</p>
+            <p className="last-message">Hello..!</p>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
