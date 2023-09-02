41106e2e34222cdc58b9756fb7314459a39b71fd
Archit Ghevariya <ghevariyaarchit@gmail.com>
2023-09-02 23:04:57 +0530

    Working on Socket

--- client/src/components/ChatContainer.js
+++ client/src/components/ChatContainer.js
@@ -23,66 +23,13 @@ export default function ChatContainer() {
           <Box><p className='msgSentTime'>1.25 PM</p></Box>
         </Box>
 
-        <Box className="MessageGroup">
-          <Box className="ReceiverMsg"><p>I am Fine!</p></Box>
-          <Box><p className='msgReceiveTime'>1.25 PM</p></Box>
-        </Box>        <Box className="MessageGroup">
-          <Box className="SenderMsg"><p>Hello, how are you?</p></Box>
-          <Box><p className='msgSentTime'>1.25 PM</p></Box>
-        </Box>
-
-        <Box className="MessageGroup">
-          <Box className="ReceiverMsg"><p>I am Fine!</p></Box>
-          <Box><p className='msgReceiveTime'>1.25 PM</p></Box>
-        </Box>        <Box className="MessageGroup">
-          <Box className="SenderMsg"><p>Hello, how are you?</p></Box>
-          <Box><p className='msgSentTime'>1.25 PM</p></Box>
-        </Box>
-
-        <Box className="MessageGroup">
-          <Box className="ReceiverMsg"><p>I am Fine!</p></Box>
-          <Box><p className='msgReceiveTime'>1.25 PM</p></Box>
-        </Box>        <Box className="MessageGroup">
-          <Box className="SenderMsg"><p>Hello, how are you?</p></Box>
-          <Box><p className='msgSentTime'>1.25 PM</p></Box>
-        </Box>
-
-        <Box className="MessageGroup">
-          <Box className="ReceiverMsg"><p>I am Fine!</p></Box>
-          <Box><p className='msgReceiveTime'>1.25 PM</p></Box>
-        </Box>        <Box className="MessageGroup">
-          <Box className="SenderMsg"><p>Hello, how are you?</p></Box>
-          <Box><p className='msgSentTime'>1.25 PM</p></Box>
-        </Box>
-
-        <Box className="MessageGroup">
-          <Box className="ReceiverMsg"><p>I am Fine!</p></Box>
-          <Box><p className='msgReceiveTime'>1.25 PM</p></Box>
-        </Box>        <Box className="MessageGroup">
-          <Box className="SenderMsg"><p>Hello, how are you?</p></Box>
-          <Box><p className='msgSentTime'>1.25 PM</p></Box>
-        </Box>
-
-        <Box className="MessageGroup">
-          <Box className="ReceiverMsg"><p>I am Fine!</p></Box>
-          <Box><p className='msgReceiveTime'>1.25 PM</p></Box>
-        </Box>        <Box className="MessageGroup">
-          <Box className="SenderMsg"><p>Hello, how are you?</p></Box>
-          <Box><p className='msgSentTime'>1.25 PM</p></Box>
-        </Box>
-
-        <Box className="MessageGroup">
-          <Box className="ReceiverMsg"><p>I am Fine!</p></Box>
-          <Box><p className='msgReceiveTime'>1.25 PM</p></Box>
-        </Box>        <Box className="MessageGroup">
-          <Box className="SenderMsg"><p>Hello, how are you?</p></Box>
-          <Box><p className='msgSentTime'>1.25 PM</p></Box>
-        </Box>
+        
 
         <Box className="MessageGroup">
           <Box className="ReceiverMsg"><p>I am Fine!</p></Box>
           <Box><p className='msgReceiveTime'>1.25 PM</p></Box>
         </Box>
+
       </Box>
     </>
   )
